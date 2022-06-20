65c5b97a1125b9017c97aeda18014c9cf9a38190
hafidarsyah@gmail.com <hafidarsyah@gmail.com>
2022-06-20 19:32:22 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Contact from "./components/Contact";
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from "./data/contacts.json";
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
+
   return (
     <div className="App">
-      
+      <Header />
+
+      {contacts.map((contact) => {
+        return <Contact data={contact} />;
+      })}
     </div>
   );
-}
+};
 
 export default App;
